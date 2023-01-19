6e72023cffb14ddda190514c3c87ba2146b44c1f
DESKTOP-KV9U18O\Cwng <jkmilop@gmail.com>
2023-01-19 16:22:02 -0500

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { PatosComponent } from './patos/patos.component';
@@ -14,7 +14,9 @@ import { ImagenesComponent } from './imagenes/imagenes.component';
     ImagenesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
